164a9244997585b39b175dfdbaab8922b449f4c3
rajalakshmi-gate-coaching <anand.r@rajalakshmi.edu.in>
2025-12-11 18:22:25 +0530

    Create firebase_config.js

---
+++ js/firebase_config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, query, where, orderBy } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAmKDzGKE9FV9A7wHG21yAbgzIYMep4iHo",
+  authDomain: "rec-gate-2026.firebaseapp.com",
+  projectId: "rec-gate-2026",
+  storageBucket: "rec-gate-2026.firebasestorage.app",
+  messagingSenderId: "966656816540",
+  appId: "1:966656816540:web:f07b7b6e2cb27114cc8459"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Export functions for use in other files
+export { db, collection, addDoc, getDocs, query, where, orderBy };
